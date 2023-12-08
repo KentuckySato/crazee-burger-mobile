42454f34a0ce315d8f43416381a3523375d0ddd8
Kentucky SATO <kentucky.sato@gmail.com>
2023-12-08 14:28:11 +0100

    📦 Remove unused dependencies and Firebase configuration

--- src/api/firebase-config.ts
+++ src/api/firebase-config.ts
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app"
+// import { initializeApp } from "firebase/app"
 // import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
+// import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_I,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-}
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_I,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig) // connect to firebase
-export const db = getFirestore(app) // get db
+// const app = initializeApp(firebaseConfig); // connect to firebase
+// export const db = getFirestore(app); // get db
 // const analytics = getAnalytics(app)
--- src/api/product.ts
+++ src/api/product.ts
@@ -1,22 +1,27 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore"
-import { db } from "./firebase-config"
-import { Products } from "../enums/product"
+// import { doc, getDoc, updateDoc } from "firebase/firestore"
+// import firestore from '@react-native-firebase/firestore';
+// import {db} from './firebase-config';
+import {Products} from '../enums/product';
 
 export const syncBothMenu = async (userId: string, menuUpdated: Products) => {
-    const docRef = doc(db, "users", userId)
+  // const docRef = doc(db, "users", userId)
+  const docRef = firestore().collection('users').doc(userId);
 
-    // Update only menu and keep others data safe.
-    await updateDoc(docRef, {
-        menu: menuUpdated,
-    })
-}
+  // firestore().collection('users').doc(userId)
+
+  // Update only menu and keep others data safe.
+  await updateDoc(docRef, {
+    menu: menuUpdated,
+  });
+};
 
 export const getMenu = async (userId: string) => {
-    const docRef = doc(db, "users", userId)
+  const docRef = doc(db, 'users', userId);
+  firestore().collection('users').doc(userId);
 
-    const docSnapshot = await getDoc(docRef)
-    if (docSnapshot.exists()) {
-        const { menu } = docSnapshot.data()
-        return menu as Products
-    }
-}
+  const docSnapshot = await getDoc(docRef);
+  if (docSnapshot.exists()) {
+    const {menu} = docSnapshot.data();
+    return menu as Products;
+  }
+};
