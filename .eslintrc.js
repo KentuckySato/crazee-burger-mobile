8a8e485ba7786c4b1abf9d245f337e9ef6810d43
Kentucky SATO <kentucky.sato@gmail.com>
2023-12-12 18:09:42 +0100

    🔧 F01: Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,13 @@ module.exports = {
   root: true,
   extends: '@react-native',
   indent: ['error', 4],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {allowConstantExport: true},
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-console': ['error', {allow: ['warn', 'error']}],
+  },
 };
